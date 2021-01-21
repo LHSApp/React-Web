ceebb15c24c714faf87629ba62b81c951ff5ac94
Leandro <leandrosantos020788@gmail.com>
2021-01-20 22:54:55 -0300

    Update

--- appdesafio/src/App.js
+++ appdesafio/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie'
 
 
 
@@ -11,6 +12,7 @@ export default () => {
 
 
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
   useEffect(()=>{
 const loadAll = async () => {
 let list = await Tmdb.getHomeList();
@@ -25,6 +27,11 @@ loadAll();
   return (
   
      <div className="page">
+
+     {featuredData &&
+      <FeaturedMovie item={featuredData}/>
+     
+     }
       <section className="lists">
         {movieList.map((item, key)=> (
 
