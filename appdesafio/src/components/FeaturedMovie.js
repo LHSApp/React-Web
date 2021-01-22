ca61446425fb53dbd0bf8173befdb9d5873d440c
Leandro <leandrosantos020788@gmail.com>
2021-01-22 17:15:21 -0300

    Update

--- appdesafio/src/components/FeaturedMovie.js
+++ appdesafio/src/components/FeaturedMovie.js
@@ -3,6 +3,14 @@ import "./FeaturedMovie.css";
 
 export default ({ item }) => {
   console.log(item);
+
+
+   let firstDate = new Date(item.first_air_date);
+   let genres = [];
+   for(let i in item.genres){
+    genres.push(item.genres[i].name);
+
+   }
   return (
     <section
       className="featured"
@@ -17,7 +25,7 @@ export default ({ item }) => {
           <div className="featured--name">{item.original_name}</div>
           <div className="featured--info">
             <div className="featured--points">{item.vote_average} Pontos</div>
-            <div className="featured--year">2099</div>
+            <div className="featured--year">{firstDate.getFullYear()}</div>
             <div className="featured--seasons">
               {item.number_of_seasons} Temporada
               {item.number_of_seasons !== 1 ? "s" : ""}
@@ -27,7 +35,7 @@ export default ({ item }) => {
 
 
             </div>
-            <div className="featured--genres"><strong>Generos:</strong>...</div>
+            <div className="featured--genres"><strong>Generos:</strong> {genres.join(',') }</div>
             
           </div>
         </div>
