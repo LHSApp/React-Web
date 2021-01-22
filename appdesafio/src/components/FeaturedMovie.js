f595051da97f2ea32fae0e27916294b168636128
Leandro <leandrosantos020788@gmail.com>
2021-01-22 18:01:38 -0300

    Update

--- appdesafio/src/components/FeaturedMovie.js
+++ appdesafio/src/components/FeaturedMovie.js
@@ -32,8 +32,8 @@ export default ({ item }) => {
             </div>
             <div className="featured--description">{item.overview}</div>
             <div className="featured--buttons">
-            <a href={`/watch/${item.id}`}>▸Assistir</a>
-            <a href={`/list/add${item.id}`}>+ Minha Lista</a>
+            <a href={`/watch/${item.id}`} className="featured--watchbutton">➤ Assistir</a>
+            <a href={`/list/add${item.id}`} className="featured--mylistbutton">+ Minha Lista</a>
 
 
             </div>
