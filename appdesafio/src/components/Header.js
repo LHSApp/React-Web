c0a49076814f3175aba9fc8a85338cdfa5a62ea8
Leandro <leandrosantos020788@gmail.com>
2021-01-23 18:30:27 -0300

    Update

--- appdesafio/src/components/Header.js
+++ appdesafio/src/components/Header.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
 
+export default () => {
+  return (
+    <header>
+      <div className="header--logo">
+        <a href="/">
+          <img
+            src="https://logodownload.org/wp-content/uploads/2014/10/netflix-logo-5.png"
+            alt="Netflix"
+          />
+        </a>
+      </div>
+      <div className="header--user">
 
-export default () =>{
-return (
-
-  <header >
-
-  ....
+        <a href="/">
+          <img src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/1bdc9a33850498.56ba69ac2ba5b.png" alt="" />
+        </a>
+      </div>
     </header>
-);
- 
-}
+  );
+};
